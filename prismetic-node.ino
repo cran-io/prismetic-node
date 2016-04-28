b3bda985ce05f3f460207e70ca3fb554541b7cef
tomslg <tomasl@cran.io>
2016-04-28 17:26:52 -0300

    Working node with RF and parallel sensors

--- prismetic-node.ino
+++ prismetic-node.ino
@@ -1,24 +1,16 @@
 //Sensor pinout
-#define sens1 A0
-#define sens2 A1
-#define led1 4
-#define led2 5
-#define MUESTRAS 5
+//CONSTANTES
+const int MUESTRAS = 5;
+const float TRIGGER =800; //mV
+const float HISTERESIS =300; //mV
 
 //Timing variables
 long delaytimer = 0, timeOut3 = 0;
 
-//Analog readings
-int sen1antes = 0;
-int sen2antes = 0;
-int sen1 = 0;
-int sen2 = 0;
-
-//Detection parameters & variables
-int RANGO = 175, HISTERESIS = 20, gente = 5, genteAntes = 0;
-boolean ping1SS = false, ping2SS = false, ping1first = false, nose = false, nose2 = false, ping1entrada = false;
-//para los 2 con aref 5v =175 esta ok
-//aref 3v3 es mierdA
+//VARIABLES
+float sen1antes = 0, sen2antes = 0, aux, sen1, sen2,sen1mV,sen2mV;
+boolean ping1first=false, ping1SS=false, ping2SS=false, nose=false, nose2=false, ping1entrada=false;
+int gente=0,genteAntes=0;
 
 //Connection Libraries
 #include <RF24Network.h>
@@ -40,104 +32,68 @@ unsigned char EEPROMAddress = 0;
 
 void setup() {
   // analogReference(EXTERNAL);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
   delay(60);
   Serial.begin(9600);
   delaytimer = millis();
-
+Serial.println("holis");
   //Connection setup
   SPI.begin();
   radio.begin();
   network.begin(90, this_node);
   payload.totalPeopleInside = EEPROM.read(EEPROMAddress);
   payload.deltaPeople = 0;
-
-  Serial.println("Starting sensor transceiver");
+  Serial.println("PRISMETIC by CRAN.IO");
+  Serial.print("Starting...");
+  Serial.println(gente);
+  
 }
 
 void loop() {
   if ((delaytimer - millis()) > 30) {
-    readSensors();
-    digitalWrite(led1, ((sen1antes > RANGO) && (sen1 > RANGO)));
-    digitalWrite(led2, ((sen2antes > RANGO) && (sen2 > RANGO)));
+   float readings[2][MUESTRAS];
 
-    updateDetectionVariables();
-
-    //user options
-    if (Serial.available() > 0) {
-      int input = Serial.read();
-      switch (input) {
-        case '0':
-          asm("jmp 0x0000");
-          break;
-        case '5':
-          gente = 5;
-          break;
-        default:
-          Serial.println("No es una instruccion valida");
-          break;
-      }
-    }
-
-    sen1antes = sen1;
-    sen2antes = sen2;
-
-    delaytimer = millis();
+  for (int i = 0; i < (MUESTRAS); i++) {
+    readings[0][(i)] = analogRead(A0);
+    readings[1][(i)] = analogRead(A1);
   }
 
-  //Connection update
-  network.update();                          // Check the network regularly
-
-  if (genteAntes != gente) {
-    Serial.println(gente);
-    payload.totalPeopleInside = gente;
-    EEPROM.write(EEPROMAddress, payload.totalPeopleInside);
-
-    //Post to Rx
-    RF24NetworkHeader header(other_node);
-    bool ok = network.write(header, &payload, sizeof(payload));
-    Serial.print("Sending...");
-    if (ok)
-      Serial.println("ok.");
-    else
-      Serial.println("failed.");
-
-    genteAntes = gente;
+  for (int i = 0; i < MUESTRAS; i++) {
+    if (readings[1][i] > readings[1][i + 1])
+      aux = readings[1][i];
+    readings[1][i] = readings[1][i + 1];
+    readings[1][i + 1] = aux;
   }
 
-}
-
-void readSensors() {
-  int readings[2][MUESTRAS];
-  sen1 = sen2 = 0;
-
-  for (int i = 1; i < (MUESTRAS + 1); i++) {
-    readings[0][(i - 1)] = analogRead(sens1);
-    sen1 += readings[0][(i - 1)];
-    readings[1][(i - 1)] = analogRead(sens2);
-    sen2 += readings[1][(i - 1)];
+  for (int i = 0; i < MUESTRAS; i++) {
+    if (readings[0][i] > readings[0][i + 1])
+      aux = readings[0][i];
+    readings[0][i] = readings[0][i + 1];
+    readings[0][i + 1] = aux;
   }
 
-  sen1 /= MUESTRAS ;
-  sen2 /= MUESTRAS ;
-}
+  for (int i = 1; i < (MUESTRAS - 1); i++) {
+    sen1 += readings[0][i];
+    sen2 += readings[1][i];
+  }
+  sen1 /= (MUESTRAS - 2);
+  sen2 /=  (MUESTRAS - 2);
 
-void updateDetectionVariables() {
-  if (((sen1antes > RANGO) && (sen1 > RANGO)) && (ping1SS == false)) { //DETECCION DEL SENSOR 1
+  float sen1mV = ((5 * sen1) / 1024);
+  sen1mV *= 1000;
+  
+  float sen2mV = ((5 * sen2) / 1024);
+  sen2mV *= 1000;
+  
+  if (((sen1antes > TRIGGER) && (sen1mV > TRIGGER)) && (ping1SS == false)) { //DETECCION DEL SENSOR 1
     ping1SS = true;
     if (ping2SS == false)
       ping1first = true;
   }
 
-  if (((sen2antes > RANGO) && (sen2 > RANGO)) && (ping2SS == false) ) { //DETECCION DEL SENSR 2
+  if (((sen2antes > TRIGGER) && (sen2mV > TRIGGER)) && (ping2SS == false) ) { //DETECCION DEL SENSR 2
     ping2SS = true;
   }
-
-
-  if ((ping1SS == true) && (sen1antes < (RANGO - HISTERESIS)) && (sen1 < (RANGO - HISTERESIS)) && (ping2SS == true) && (sen2antes < (RANGO - HISTERESIS)) && (sen2 < (RANGO - HISTERESIS))) {
+  if ((ping1SS == true) && (sen1antes < (TRIGGER - HISTERESIS)) && (sen1mV < (TRIGGER - HISTERESIS)) && (ping2SS == true) && (sen2antes < (TRIGGER - HISTERESIS)) && (sen2mV < (TRIGGER - HISTERESIS))) {
     //Set detection variables to default values
     ping1SS = false;
     ping2SS = false;
@@ -168,7 +124,7 @@ void updateDetectionVariables() {
   }
 
   //SACAR PINGS EN TRUE SI NADIE PASA
-  if (((ping1SS == true) || (ping2SS == true)) && (nose2 == false)  &&  (sen1antes < (RANGO - HISTERESIS))  &&  (sen1 < (RANGO - HISTERESIS))  &&  (sen2antes < (RANGO - HISTERESIS))  &&  (sen2 < (RANGO - HISTERESIS))  ) {
+  if (((ping1SS == true) || (ping2SS == true)) && (nose2 == false)  &&  (sen1antes < (TRIGGER - HISTERESIS))  &&  (sen1mV < (TRIGGER - HISTERESIS))  &&  (sen2antes < (TRIGGER - HISTERESIS))  &&  (sen2mV < (TRIGGER - HISTERESIS))  ) {
     nose2 = true;
     timeOut3 = millis();
   }
@@ -183,5 +139,183 @@ void updateDetectionVariables() {
       nose2 = false;
     }
   }
+  sen1antes=sen1mV;
+  sen2antes=sen2mV;
+  gente = constrain(gente, 0, 9999999);
+    //user options
+    if (Serial.available() > 0) {
+      int input = Serial.read();
+      switch (input) {
+        case '0':
+          asm("jmp 0x0000");
+          break;
+        case '5':
+          gente = 5;
+          break;
+        default:
+          Serial.println("No es una instruccion valida");
+          break;
+      }
+    }
+
+    
+    delaytimer = millis();
+  }
+
+  //Connection update
+  network.update();                          // Check the network regularly
+
+    if (genteAntes < gente) {
+      Serial.print("ENTRADA. TOTAL: ");
+      Serial.println(gente);
+       Serial.println(gente);
+    payload.totalPeopleInside = gente;
+    EEPROM.write(EEPROMAddress, payload.totalPeopleInside);
+
+    //Post to Rx
+    RF24NetworkHeader header(other_node);
+    bool ok = network.write(header, &payload, sizeof(payload));
+    Serial.print("Sending...");
+    if (ok)
+      Serial.println("ok.");
+    else
+      Serial.println("failed.");
+      genteAntes = gente;
+    }
+    else if(genteAntes > gente){
+      Serial.print("SALIDA. TOTAL: ");
+      Serial.println(gente);
+       Serial.println(gente);
+    payload.totalPeopleInside = gente;
+    EEPROM.write(EEPROMAddress, payload.totalPeopleInside);
+
+    //Post to Rx
+    RF24NetworkHeader header(other_node);
+    bool ok = network.write(header, &payload, sizeof(payload));
+    Serial.print("Sending...");
+    if (ok)
+      Serial.println("ok.");
+    else
+      Serial.println("failed.");
+      genteAntes = gente;
+    }
+    
+
+    if (Serial.available() > 0) {
+      int input = Serial.read();
+      switch (input) {
+        case '0':
+          asm("jmp 0x0000");
+          break;
+        case '5':
+          gente = genteAntes = 5;          
+          Serial.println("SET TO 5");
+          break;
+        default:
+          Serial.println("No es una instruccion valida");
+          break;
+      }
+    }
+
 }
+
+void readSensors() {
+    float readings[2][MUESTRAS];
+
+  for (int i = 0; i < (MUESTRAS); i++) {
+    readings[0][(i)] = analogRead(A0);
+    readings[1][(i)] = analogRead(A1);
+  }
+
+  for (int i = 0; i < MUESTRAS; i++) {
+    if (readings[1][i] > readings[1][i + 1])
+      aux = readings[1][i];
+    readings[1][i] = readings[1][i + 1];
+    readings[1][i + 1] = aux;
+  }
+
+  for (int i = 0; i < MUESTRAS; i++) {
+    if (readings[0][i] > readings[0][i + 1])
+      aux = readings[0][i];
+    readings[0][i] = readings[0][i + 1];
+    readings[0][i + 1] = aux;
+  }
+
+  for (int i = 1; i < (MUESTRAS - 1); i++) {
+    sen1 += readings[0][i];
+    sen2 += readings[1][i];
+  }
+  sen1 /= (MUESTRAS - 2);
+  sen2 /=  (MUESTRAS - 2);
+
+  float sen1mV = ((5 * sen1) / 1024);
+  sen1mV *= 1000;
+  
+  float sen2mV = ((5 * sen2) / 1024);
+  sen2mV *= 1000;
+
+}
+
+void updateDetectionVariables() {
+  if (((sen1antes > TRIGGER) && (sen1mV > TRIGGER)) && (ping1SS == false)) { //DETECCION DEL SENSOR 1
+    ping1SS = true;
+    if (ping2SS == false)
+      ping1first = true;
+  }
+
+  if (((sen2antes > TRIGGER) && (sen2mV > TRIGGER)) && (ping2SS == false) ) { //DETECCION DEL SENSR 2
+    ping2SS = true;
+  }
+  if ((ping1SS == true) && (sen1antes < (TRIGGER - HISTERESIS)) && (sen1mV < (TRIGGER - HISTERESIS)) && (ping2SS == true) && (sen2antes < (TRIGGER - HISTERESIS)) && (sen2mV < (TRIGGER - HISTERESIS))) {
+    //Set detection variables to default values
+    ping1SS = false;
+    ping2SS = false;
+    ping1first = false;
+    nose = false;
+  }
+
+  if (((ping2SS == true) && (ping1SS == true) && (nose == false))) {
+    nose = true;
+    nose2 = false;
+    ping1SS = false;
+    ping2SS = false;
+    if (ping1first == true) {
+      if (ping1entrada == true) {
+        gente++ ;
+      } else {
+        gente--;
+      }
+    } else {
+      if (ping1entrada == true) {
+        gente--;
+      } else {
+        gente++;
+      }
+    }
+    ping1first = false;
+    gente = constrain(gente, 0, 9999999);
+  }
+
+  //SACAR PINGS EN TRUE SI NADIE PASA
+  if (((ping1SS == true) || (ping2SS == true)) && (nose2 == false)  &&  (sen1antes < (TRIGGER - HISTERESIS))  &&  (sen1mV < (TRIGGER - HISTERESIS))  &&  (sen2antes < (TRIGGER - HISTERESIS))  &&  (sen2mV < (TRIGGER - HISTERESIS))  ) {
+    nose2 = true;
+    timeOut3 = millis();
+  }
+
+
+  if (((ping1SS == true) || (ping2SS == true) ) && (nose2 == true)) {
+    if ((millis() - timeOut3) > 1000 ) {
+      ping1SS = false;
+      ping2SS = false;
+      ping1first = false;
+      nose = false;
+      nose2 = false;
+    }
+  }
+  sen1antes=sen1mV;
+  sen2antes=sen2mV;
+  gente = constrain(gente, 0, 9999999);
+  }
+  
+
 
