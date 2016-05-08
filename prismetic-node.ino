23abb384463d615c2fac269bdae8549316752515
Javier Velazquez Traut <javier@cran.io>
2016-05-07 23:22:56 -0300

    Posting every 30sec. People IN and people OUT.

--- prismetic-node.ino
+++ prismetic-node.ino
@@ -1,8 +1,8 @@
 //Sensor pinout
 //CONSTANTES
-const int SENSORES = 2;
-const int MUESTRAS = 11;
-const int MUESTRAS_PROMEDIADAS = 5;
+const unsigned int SENSORES = 2;
+const unsigned int MUESTRAS = 11;
+const unsigned int MUESTRAS_PROMEDIADAS = 5;
 const float TRIGGER = 60; //Cada unidad son 5mV aprox
 const float HISTERESIS = 20; //Cada unidad son 5mV aprox
 const unsigned int TRIGGER_DUR_MIN = 25; //ms
@@ -10,14 +10,14 @@ const unsigned int TRIGGER_DIFF_MAX = 500; //ms
 const unsigned int TRIGGER_DELAY_MAX = 5000; //ms
 
 //Timing variables
-long readTimer = 0;
+const unsigned int MUESTRAS_PERIOD = 1; //1ms
+unsigned long readTimer = 0;
 
 //VARIABLES
 float sen1antes = 0, sen2antes = 0;
 long sen1Timer = 0, sen2Timer = 0;
 unsigned int sen1Duration = 0, sen2Duration = 0;
 boolean ping1SS = false, ping2SS = false;
-int gente = 0, genteAntes = 0;
 
 //LECTURAS
 unsigned int readings[SENSORES][MUESTRAS];
@@ -36,37 +36,39 @@ RF24Network network(radio);          // Network uses that radio
 const uint16_t this_node = 01;        // Address of our node in Octal format
 const uint16_t other_node = 00;       // Address of the other node in Octal format
 struct payload_t {                  // Structure of our payload
-  unsigned char totalPeopleInside;
-  signed char deltaPeople;
+  long totalPeopleInside;
+  int peopleIn;
+  int peopleOut;
 };
 payload_t payload;
 unsigned char EEPROMAddress = 0;
+unsigned long postTimer = 0;
+const unsigned int POST_PERIOD = 30000;
+const unsigned int POST_RETRY = 5000;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("PRISMETIC by CRAN.IO");
-  Serial.print("Starting...");
+  Serial.print("Starting... ");
   SPI.begin();
   radio.begin();
   network.begin(90, this_node);  //crear la network
-  gente = EEPROM.read(EEPROMAddress); //inicializar la gente que hay adentro (guardado en EEPROM)
-  payload.totalPeopleInside = gente;
-  payload.deltaPeople = 0;
-  Serial.println(gente);
+  payload.totalPeopleInside = EEPROM.read(EEPROMAddress); //inicializar la gente que hay adentro (guardado en EEPROM)
+  payload.peopleIn = 0;
+  payload.peopleOut = 0;
+  Serial.print("TOTAL: ");
+  Serial.println(payload.totalPeopleInside);
   delay(60);                                                          //Warm-up del sensor
-  
-  for (int i = 1; i < MUESTRAS; i++){                                     //Init readings
+  for (int i = 1; i < MUESTRAS; i++){                                 //Init readings
     readings[0][i] = analogRead(A0);
     readings[1][i] = analogRead(A1);
   }
   readingsFloor[0] = readings[0][readIndex];
   readingsFloor[1] = readings[1][readIndex];
-  
-  readTimer = millis();
 }
 
 void loop() {
-  if ((millis() - readTimer) > 1) {                                   //Adquisicion de datos cada 1ms, 11 muestras de cada sensor son 11ms, se ordenan de mayor a menor y se promedian las 5 del medio
+  if (millis() > readTimer) {                                   //Adquisicion de datos cada 1ms, 11 muestras de cada sensor son 11ms, se ordenan de mayor a menor y se promedian las 5 del medio
     readings[0][readIndex] = analogRead(A0);
     readings[1][readIndex] = analogRead(A1);
     
@@ -136,24 +138,31 @@ void loop() {
 
     if ((ping1SS == false ) && (ping2SS == false) && (sen1Timer > 0) && (sen2Timer > 0)) { //Si ya bajaron los sensores
       Serial.print("Sensor 1 - Timer:");
-        Serial.print(sen1Timer);
-        Serial.print(", Duration: ");
-        Serial.print(sen1Duration);
-        Serial.println(".");
-        Serial.print("Sensor 2 - Timer: ");
-        Serial.print(sen2Timer);
-        Serial.print(", Duration: ");
-        Serial.print(sen2Duration);
-        Serial.println(".");
+      Serial.print(sen1Timer);
+      Serial.print(", Duration: ");
+      Serial.print(sen1Duration);
+      Serial.println(".");
+      Serial.print("Sensor 2 - Timer: ");
+      Serial.print(sen2Timer);
+      Serial.print(", Duration: ");
+      Serial.print(sen2Duration);
+      Serial.println(".");
       if ((abs(sen1Timer - sen2Timer) < TRIGGER_DIFF_MAX) && (sen1Duration > TRIGGER_DUR_MIN) && (sen2Duration > TRIGGER_DUR_MIN)) {
         //Si la diferencia entre los sensores es menor que TRIGGER_DIFF_MAX y la duracion de los pulsos es mas que TRIGGER_DUR_MIN
-        if ( sen1Timer < sen2Timer)
-          gente++ ;
-        else
-          gente--;
-
-        if (gente < 0)
-          gente = 0;
+        if ( sen1Timer < sen2Timer){
+          payload.peopleIn++ ;
+          payload.totalPeopleInside++;
+          Serial.print("IN. TOTAL: ");
+          Serial.println(payload.totalPeopleInside);
+        }
+        else{
+          payload.peopleOut++;
+          payload.totalPeopleInside--;
+          Serial.print("OUT. TOTAL: ");
+          Serial.println(payload.totalPeopleInside);
+        }
+        if(payload.totalPeopleInside > 0)
+          EEPROM.write(EEPROMAddress, payload.totalPeopleInside);
       }
       sen1Timer = 0;
       sen2Timer = 0;
@@ -171,50 +180,57 @@ void loop() {
     if (readIndex >= MUESTRAS) {
       readIndex = 0;
     }
-    readTimer = millis();
+    readTimer = millis() + MUESTRAS_PERIOD;
   }
 
   //Connection update
   network.update();                          // Check the network regularly
 
-  if (genteAntes != gente) {
-    if (gente > genteAntes)
-      Serial.print("ENTRADA.");
+  if (millis() > postTimer) {
+    bool success = post();
+    if(success)
+      postTimer = millis() + POST_PERIOD;
     else
-      Serial.print("SALIDA.");
-    Serial.print("TOTAL: ");
-    Serial.println(gente);
-    payload.totalPeopleInside = gente;
-    EEPROM.write(EEPROMAddress, gente);
-
-    //Post to Rx
-    RF24NetworkHeader header(other_node);
-    bool ok = network.write(header, &payload, sizeof(payload));
-    Serial.print("Sending...");
-    if (ok) {
-      genteAntes = gente;
-      Serial.println("ok.");
-    }
-    else {
-      Serial.println("failed.");
-    }
-    genteAntes = gente;
+      postTimer = millis() + POST_RETRY;
   }
 
   if (Serial.available() > 0) {                       //opciones por serial para resetear o establecer la gente en 5 para pruebas
     int input = Serial.read();
     switch (input) {
       case '0':
-        asm("jmp 0x0000");
+        resetPeople();
         break;
-      case '5':
-        gente = genteAntes = 5;
-        Serial.println("SET TO 5");
+      case 'p':
+        post();
         break;
       default:
-        Serial.println("No es una instruccion valida");
+        Serial.println("No es una instrucción valida");
         break;
     }
   }
+}
+
+bool post(){
+  //Post to Rx
+  RF24NetworkHeader header(other_node);
+  bool success = network.write(header, &payload, sizeof(payload));
+  Serial.print("Sending... ");
+  if (success) {
+    payload.peopleIn = 0;
+    payload.peopleOut = 0;
+    Serial.println("post OK.");
+  }
+  else {
+    Serial.println("post Failed.");
+  }
+  return success;
+}
+
+void resetPeople(){
+  payload.totalPeopleInside = 0;
+  payload.peopleIn = 0;
+  payload.peopleOut = 0;
+  EEPROM.write(EEPROMAddress, payload.totalPeopleInside);
+  Serial.println("RESET PEOPLE TO 0");
 }
 
